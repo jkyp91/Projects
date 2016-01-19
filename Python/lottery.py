9c471761fc9cd54a87af706be21dae6f5c5ad13a
jkyp <john.k.x@hotmail.co.uk>
2016-01-19 23:49:58 +0000

    Update lottery.py

--- Python/lottery.py
+++ Python/lottery.py
@@ -1,5 +1,7 @@
 import random
-
+#ALTERNATIVE LIST COMPREHENSION (this is practice with functions):
+#print[random.randint(1,50) for i in range(5)]
+#the above would simply provide 5 numbers..Crazy simple compared to the following:
 #generate 5 numbers between 1-50
 def five_numbers():
   numbers = []
